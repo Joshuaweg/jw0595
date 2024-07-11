6def0248e0659683b0c2f7ca6fc132557479d3f0
Joshua Weg <Joshua.weg.95@gmail.com>
2024-07-11 12:13:09 -0700

    Update RentalAgreement.java

--- src/main/java/RentalAgreement.java
+++ src/main/java/RentalAgreement.java
@@ -3,7 +3,6 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.DayOfWeek;
-//import java.time.Calendar;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.text.NumberFormat;
@@ -11,6 +10,8 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+//Generates the Agreement between the customer and clerk
+//takes in parameters item code, date, days to rent, and discount for constructor
 public class RentalAgreement{
 
 	String code;
@@ -45,6 +46,7 @@ public class RentalAgreement{
 		accepted = false;
 		
 	}
+	//Constructor that matches input parameters from test
 	public RentalAgreement(String code,String date, int days, int discount){
 		ToolList tools = new ToolList();
 		tools.buildList();
@@ -55,6 +57,9 @@ public class RentalAgreement{
 		this.discount=discount;
 		checkoutDate = processDate(date);
 		dueDate = checkoutDate.plusDays(days);
+		//accepted will track whether any entry is invalid based on known criteria
+		//days must be greater than 0
+		//discount must be between 0-100
 		accepted =!(discount<0||discount>100||days<1);
 		chargeDays = calculateChargeDays(tools, charges);
 		dailyCharge = getDailyCharge(tools, charges);
@@ -62,11 +67,13 @@ public class RentalAgreement{
 		discountAmount = calculateDiscount();
 		finalCharge =preDiscount-discountAmount;	
 	}
+	//Converts date string to LocalDate object
 	 public LocalDate processDate(String date){
 		 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yy");
 		 LocalDate dt = LocalDate.parse(date,formatter);
 		 return dt;
 	 }
+	//Calculates the actual charge days based on tools and charges objects
 	 private int calculateChargeDays(ToolList tools, ChargeList charges){
 		 int daysDeducted = 0;
 		 Tool current = tools.get(code);
@@ -75,14 +82,17 @@ public class RentalAgreement{
 		 boolean weekday = chrg.getWeekday();
 		 boolean weekend = chrg.getWeekend();
 		 boolean holiday = chrg.getHoliday();
+		 //holidays deducted if holidays not charged
 		 if(!holiday){
 			 daysDeducted+=deductHoliday();
 		 }
+		 //weekends deducted when weekends not charged
 		 if(!weekend){
 			 daysDeducted+=deductWeekend();
 		 }
 		 return days-daysDeducted;
 	 }
+	//Deducts days based on whether day is holiday
 	 private int deductHoliday(){
 		int day = 0;
 		boolean isJuly = checkoutDate.getMonth()==Month.JULY||dueDate.getMonth()==Month.JULY;
@@ -102,6 +112,7 @@ public class RentalAgreement{
 		
 		return day;
 	 }
+	 //deducts days when day is weekend
 	 private int deductWeekend(){
 		int day = 0;
 		for(LocalDate dt = checkoutDate; !dt.isAfter(dueDate); dt=dt.plusDays(1)){
@@ -118,6 +129,7 @@ public class RentalAgreement{
 	 public float getFinalCharge(){
 		 return finalCharge;
 	 }
+	//Prints out agreement or prints out message that agreement parameters were invalid
 	 public void print(){
 		 if(accepted){
 		 NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(Locale.US);
@@ -139,9 +151,11 @@ public class RentalAgreement{
 		 System.out.println("Final charge: "+currencyFormatter.format(finalCharge));
 		 }
 		 else{
+			 //message if days is less than 1
 			 if(days<1){
 				System.out.println("Days to rent is less than 1. Please try again"); 
 			 }
+		        //message if discount is not in range 0-100
 			 else{
 				 System.out.println("Discount percentage is not within range of 0-100. Please try again");
 			 }
