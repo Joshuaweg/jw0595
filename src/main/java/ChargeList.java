20e128c76d7e6376da66278555ff63c8022dddd2
Joshua Weg <Joshua.weg.95@gmail.com>
2024-07-11 11:58:41 -0700

    Update ChargeList.java

--- src/main/java/ChargeList.java
+++ src/main/java/ChargeList.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
+//Builds to records from charges.tsv
 public class ChargeList{
 	final static String CHARGESPATH = "src/main/charges.tsv";
 	private Map<String, Charge> chargeList;
@@ -14,6 +14,7 @@ public class ChargeList{
 		chargeList = new HashMap<>();
 		toolNames = new ArrayList<>();
 	}
+	//Driving Method that builds all charge entries
 	public void buildList(){
 
 		try(Scanner scanner = new Scanner(new File(CHARGESPATH))){
@@ -43,6 +44,7 @@ public class ChargeList{
 
 
 	}
+	//method that returns specific charge
 	public Charge get(String type){
 		return chargeList.get(type);
 	}
